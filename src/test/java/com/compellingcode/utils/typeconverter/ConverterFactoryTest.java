d968672921d1fc0bb9abb04c56592ad2414457b9
Michael Slate <michael@codeguy.org>
2018-03-30 04:13:18 +0000

    removed target/* and tweaked factory unit test

--- src/test/java/com/compellingcode/utils/typeconverter/ConverterFactoryTest.java
+++ src/test/java/com/compellingcode/utils/typeconverter/ConverterFactoryTest.java
@@ -1,11 +1,15 @@
 package com.compellingcode.utils.typeconverter;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import com.compellingcode.utils.typeconverter.converters.StringConverter;
+import com.compellingcode.utils.typeconverter.exception.UnknownTypeException;
 
 import junit.framework.TestCase;
 
+@RunWith(JUnit4.class)
 public class ConverterFactoryTest extends TestCase {
 	
 	@Test
@@ -15,6 +19,13 @@ public class ConverterFactoryTest extends TestCase {
 		Converter c = cf.getConverter(String.class);
 		
 		assertTrue(c instanceof StringConverter);
+	}
+	
+	@Test(expected=UnknownTypeException.class)
+	public void testInvalidConverter() throws Exception {
+		ConverterFactory cf = new ConverterFactory();
+		
+		cf.getConverter(Test.class);
 	}
 	
 }
